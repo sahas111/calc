0200b37f9f4b7801342f0375ca81672950799003
sahas <susmi06@yahoo.com>
2015-03-05 15:27:56 +1100

    changed
    test test

---
+++ calc.py
@@ -0,0 +1,38 @@
+""""calc.py: A simple Python calculator changed."""
+
+import sys
+
+def add_all(nums):
+    return sum(nums)
+
+def multiply_all(nums):
+    return reduce(lambda a, b: a * b, nums)
+
+
+def divide_all(nums):
+    return reduce(lambda a, b: a / b, nums)
+
+def diff_all(nums):
+    return reduce(lambda a, b: a - b, nums)
+
+
+if __name__ == '__main__':
+    command = sys.argv[1]
+    nums = map(float, sys.argv[2:])
+    if command == 'add':
+        print(add_all(nums))
+    elif command == 'subtract':
+        print(diff_all(nums))
+    elif command == 'multiply':
+        print(multiply_all(nums))
+    elif command == 'divide':
+        print(divide_all(nums))
+    elif command == 'min':
+        print(min(nums))
+    elif command == 'max':
+        print(max(nums))
+    elif command == 'int':
+        print(int(nums))    
+    else:
+        usage = "calc.py [add|multiply|min][max] NUM1 [NUM2 [NUM3 [...]]]"
+        print(usage)
